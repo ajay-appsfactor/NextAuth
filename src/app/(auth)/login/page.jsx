aee908933fccc1ec670c282312fde0d514f6cd86
ajay-appsfactor <ajay.appsfactor@gmail.com>
2025-06-27 18:45:48 +0530

    fix bugs

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -19,8 +19,8 @@ export default function LoginPage() {
   const formik = useFormik({
     initialValues: { email: "", password: "" },
     validationSchema: Yup.object({
-      email: Yup.string().email("Invalid email").required("Email is qequired"),
-      password: Yup.string().required("Password is required"),
+      email: Yup.string().email("Invalid email").required("Email is required"),
+      password: Yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
     }),
     onSubmit: async (values) => {
       setServerError("");
